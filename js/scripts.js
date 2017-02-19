9febf052b6a2d8f3666a788c7f2d16f64de4aa26
jbyrnesshay <jmb464@g.harvard.edu>
2017-02-18 19:06:42 -0500

    removed faulty css to fix behavior of burger rotation on ul click

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 
+	/* sameHeights script, courtesy of ... */
+
 	if ('addEventListener' in window) {
 	    window.addEventListener('resize', function(){
 	        sameHeights();
@@ -7,8 +9,6 @@
 	        sameHeights();
 	    });
 	}
-
-
 
 	function sameHeights(selector) {
 	    var selector = selector || '[data-key="sameHeights"]',
@@ -30,37 +30,62 @@
 	    }
 	}
  
-function waytoGo(badthings) {
-	var text = badthings || 'hello there is my default';
-	alert(text);
-}
+
+	/* hamburger drop-down menu and dd menu behaviorcontrol*/
+	/* logic by myself */
+	/* basis derived from .. */
+
 function dropper() {
-
-	var tots = document.getElementsByClassName(name);
-	var name = 'dropcontent';
-	 var tots = document.getElementsByClassName(name);
 	 
- 	 	
- 	 document.onclick = function(e){
+	var name = 'dropcontent';
+	var tots = document.getElementsByClassName(name);
+	var burger = document.getElementById("clickme");
+ 	/*onclick code controls the following behavior- opening the menu on button click only,
+ 	closing the menu on either button click or anywhere document click, while disabling
+ 	the closing behavior on clicks on the links in the dropdown menu itself, as well
+ 	as coordinating the rotation of the burger display when menu either open or closed */
 
- 	  
- 	  	whereClicked = e.target;
- 	  	//alert(whereClicked);		is it link										is it button
- 	 	if (!(document.getElementById("dropking").contains(whereClicked))){
- 	 		 
-
- 	 	 	if(tots[0].classList.contains('show')) 
- 	 		{ 
+ 	document.onclick = function(e){
+		whereClicked = e.target;
+		/*anywhere on document outside dropdown scheme(not button, not menu)*/
+ 	 	if (!(document.getElementById("dropking").contains(whereClicked))) {
+ 	 		//alert('a');
+ 	 		if(tots[0].classList.contains('show')) { 
+ 	 		/*if outside dropdown scheme and menu is open */
+ 	 			 
  	 			tots[0].classList.remove('show');
- 	 		 
- 	 		}}
- 	 		
-		else if (!(document.getElementById("fa").contains(whereClicked))){
-			 
-		tots[0].classList.toggle('show');	}
-			 }
-		 
+ 	 			burger.classList.remove('rotate');
+ 	 		}
+ 	 	}
+ 	 	/*button click logic only, ie, not outside dropdown, and not dropdown UL*/
+		else if (!(document.getElementById("fa").contains(whereClicked))) {
+			//alert('ah');
+			burger.classList.toggle('rotate');
+			if (!(tots[0].classList.contains('show'))) {
+				/*if button clicked and menu not open */
+				tots[0].classList.add('show');
+			}
+			else {
+			//alert ('ah');
+			/*if button clicked and menu open*/
+				tots[0].classList.toggle('show');
+			}
+		}
 	}
+}
+		/*THIS DOESNT REALLY WORK AT MOMENT-this is just a cosmetic fix to annoying premature glimpse of hburger menu
+		when playing with resize window back and forth */
+		/*that I have this problem behavior has to do with my using css transitions  */
+		/*i am seeing the transition on resize from higher values down to 479 */
+		window.addEventListener('resize', fix);
+ 		function fix() {
+ 			if ((document.documentElement.clientWidth >479)) 
+ 			{ 
+ 			 	tots[0].classList.remove('show');
+ 			}
+ 		}
+ 	
+
  	 	
  	 
  	 
